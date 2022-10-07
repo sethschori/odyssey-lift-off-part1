eb04bfb16d099f165ebbf604fb6e522dfbb6f1cd
Seth Schori <20755795+sethschori@users.noreply.github.com>
2022-10-07 15:32:30 -0400

    Map tracks and render TrackCard components

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Layout } from '../components';
 import {gql, useQuery} from '@apollo/client';
+import TrackCard from '../containers/track-card';
 
 const TRACKS = gql`
   query GetTracks {
@@ -30,7 +31,11 @@ const Tracks = () => {
 
   if (error) return `ERROR! ${error.message}`;
 
-  return <Layout grid>{JSON.stringify(data)}</Layout>;
+  return <Layout grid>
+    {data?.tracksForHome?.map((track) => (
+      <TrackCard key={track.id} track={track} />
+    ))}
+  </Layout>;
 };
 
 export default Tracks;
