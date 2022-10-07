320c3c90d1aff2880a501e34dd16f22acf259608
Seth Schori <20755795+sethschori@users.noreply.github.com>
2022-10-07 11:03:45 -0400

    Add schema Track, Author types and tracksForHome query

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,5 +1,34 @@
 const {gql} = require('apollo-server')
 
-const typeDefs = gql``;
+const typeDefs = gql`
+type Query {
+    "Query to get tracks array for the homepage grid"
+    tracksForHome:[Track!]!
+}
+
+"A track is a group of learning Modules that teaches about a specific topic"
+type Track {
+    id: ID!
+    "the track's title"
+    title: string!
+    "the track's main author"
+    author: Author!
+    "the track's main illustration to disaply in track card or track page detail"
+    thumbnail: String
+    "the track's approximate length to complete, in minutes"
+    length: Int
+    "the number of modules this track contains"
+    modulesCount: Int
+}
+
+"Author of a complete Track"
+type Author {
+    id: ID!
+    "Author's first and last name"
+    name: string!
+    "Author's profile picture url"
+    photo: string
+}
+`;
 
 module.exports = typeDefs;
