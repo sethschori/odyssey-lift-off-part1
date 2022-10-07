b2b427db33eacb691a16c1870f7bd4cc291eba28
Seth Schori <20755795+sethschori@users.noreply.github.com>
2022-10-07 15:39:42 -0400

    Wrap with <QueryResult> instead of returning strings

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Layout } from '../components';
 import {gql, useQuery} from '@apollo/client';
 import TrackCard from '../containers/track-card';
+import QueryResult from '../components/query-result';
 
 const TRACKS = gql`
   query GetTracks {
@@ -27,14 +28,12 @@ const TRACKS = gql`
 const Tracks = () => {
   const { loading, error, data } = useQuery(TRACKS);
 
-  if (loading) return 'Loading...';
-
-  if (error) return `ERROR! ${error.message}`;
-
   return <Layout grid>
-    {data?.tracksForHome?.map((track) => (
-      <TrackCard key={track.id} track={track} />
-    ))}
+    <QueryResult error={error} loading={loading} data={data}>
+      {data?.tracksForHome?.map((track) => (
+        <TrackCard key={track.id} track={track} />
+      ))}
+    </QueryResult>
   </Layout>;
 };
 
