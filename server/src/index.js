ab1a0ada2515480bbbf2078b2c69d88d2930da74
Seth Schori <20755795+sethschori@users.noreply.github.com>
2022-10-07 11:34:42 -0400

    Start ApolloServer

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,12 @@
-//TODO
+const {ApolloServer} = require('apollo-server');
+const typeDefs = require('./schema');
+
+const server = new ApolloServer({ typeDefs });
+
+server.listen().then(() => {
+  console.log(`
+    Server is running!
+    Listening on port 4000
+    Query at http://localhost:4000
+  `);
+})
